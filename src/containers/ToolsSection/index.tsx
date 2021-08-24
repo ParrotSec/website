aa25ab24bccb7ead7ef00a18d98f35f257cd6a49
Vladyslav Dalechyn <vlad.dalechin@gmail.com>
2021-08-24 15:13:23 +0300

    responsive icons to tools section

--- src/containers/ToolsSection/index.tsx
+++ src/containers/ToolsSection/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Box, /*Box,*/ Grid, makeStyles, Paper, PaperProps, Typography } from '@material-ui/core'
+import { Box, Grid, Hidden, makeStyles, Paper, PaperProps, Typography } from '@material-ui/core'
 import cls from 'classnames'
 import tools from './assets/tools.svg'
-import parrotBg from './assets/parrotbg.png'
+import parrotBg from './assets/shellBg.png'
 import PButton from 'components/PButton'
-/*import burp from './assets/tool-logo-burp 1.svg'
+import burp from './assets/tool-logo-burp 1.svg'
 import john from './assets/tool-logo-john 1.svg'
 import maltego from './assets/tool-logo-maltego 1.svg'
 import nmap from './assets/tool-logo-nmap 1.svg'
 import sqlmap from './assets/tool-logo-sqlmap 1.svg'
-import metasploit from './assets/tool-logo-metasploit 1.svg'*/
+import metasploit from './assets/tool-logo-metasploit 1.svg'
+import { useMeasure } from 'react-use'
 
 const useStyles = makeStyles({
   root: {
@@ -19,7 +20,13 @@ const useStyles = makeStyles({
     paddingRight: 16,
     paddingBottom: 75
   },
+  headerIcon: {
+    padding: 16,
+    background: '#FFF',
+    borderRadius: 6
+  },
   icon: {
+    position: 'absolute',
     padding: 16,
     background: '#FFF',
     borderRadius: 6
@@ -28,22 +35,32 @@ const useStyles = makeStyles({
     opacity: 0.5
   },
   bigIcon: {
+    position: 'absolute',
     width: 84,
     height: 84,
     padding: 16,
     background: '#FFF',
     borderRadius: 6
+  },
+  bg: {
+    borderRadius: 6,
+    boxShadow: '0px 14px 56px 21px rgba(0, 0, 0, 0.25)'
+  },
+  bgHolder: {
+    width: 'calc(100% - 512px)',
+    '@media (max-width: 1600px)': {
+      width: '75%'
+    }
   }
 })
 
 const ToolsSection = ({ className, ...rest }: PaperProps) => {
   const classes = useStyles()
+  const [ref, { width, height }] = useMeasure()
   return (
     <Paper className={cls(classes.root, className)} {...rest}>
       <Grid container item xs={12} justifyContent="center" alignItems="center" direction="column">
-        <div>
-          <img className={classes.icon} src={tools} alt="tools icon" />
-        </div>
+        <img className={classes.headerIcon} src={tools} alt="tools icon" />
         <Typography variant="h2" align="center">
           Tools for every occasion
         </Typography>
@@ -51,25 +68,70 @@ const ToolsSection = ({ className, ...rest }: PaperProps) => {
           Choose between 600+ tools for all your hacking needs
         </Typography>
         {/*TODO: for futher animations need to apply box-shadow*/}
-        <img src={parrotBg} alt="Parrot BG" />
-        {/* <Box position="relative">
-          <img className={classes.bigIcon} src={burp} alt="Burp" />
+
+        <Hidden mdDown>
+          <Box position="relative" width={width}>
+            <img
+              className={classes.bigIcon}
+              src={burp}
+              alt="Burp"
+              style={{
+                top: -24,
+                left: 0.922 * width
+              }}
+            />
+            <img
+              className={classes.bigIcon}
+              src={john}
+              alt="John"
+              style={{
+                top: 0.902 * height,
+                left: 0.045 * width
+              }}
+            />
+            <img
+              className={classes.bigIcon}
+              width={48}
+              height={48}
+              src={maltego}
+              alt="Maltego"
+              style={{
+                left: 0.958 * width,
+                top: 0.595 * height
+              }}
+            />
+            <img
+              className={classes.icon}
+              src={nmap}
+              alt="Nmap"
+              style={{
+                top: 0.518 * height,
+                left: -0.14 * width
+              }}
+            />
+            <img
+              className={classes.icon}
+              src={metasploit}
+              alt="Metasploit"
+              style={{
+                top: 0.23 * height,
+                left: 1.111 * width
+              }}
+            />
+            <img
+              className={classes.bigIcon}
+              src={sqlmap}
+              alt="SQLmap"
+              style={{
+                top: 0.2 * height,
+                left: -42
+              }}
+            />
+          </Box>
+        </Hidden>
+        <Box className={classes.bgHolder}>
+          <img ref={ref} className={classes.bg} src={parrotBg} alt="Parrot BG" />
         </Box>
-        <Box position="relative">
-          <img className={classes.bigIcon} src={john} alt="John" />
-        </Box>
-        <Box position="relative">
-          <img className={classes.bigIcon} width={48} height={48} src={maltego} alt="Maltego" />
-        </Box>
-        <Box position="relative">
-          <img className={classes.icon} src={nmap} alt="Nmap" />
-        </Box>
-        <Box position="relative" left={472} top={-460}>
-          <img className={classes.icon} src={metasploit} alt="Metasploit" />
-        </Box>
-        <Box position="relative" top={-543} right={360}>
-          <img className={classes.bigIcon} src={sqlmap} alt="SQLmap" />
-        </Box>*/}
         <Typography variant="h3" align="center">
           Explore over 600+ tools
         </Typography>
--- types/global.d.ts
+++ types/global.d.ts
@@ -6,3 +6,7 @@ declare module '*.png' {
   const content: string
   export default content
 }
+declare module '*.jpg' {
+  const content: string
+  export default content
+}
