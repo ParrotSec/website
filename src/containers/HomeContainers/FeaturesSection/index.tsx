cd0e2647b0d15307c5e6c5cf2ec309e2c9202fc7
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2021-09-28 19:20:47 +0200

    upload new texts for features section

--- src/containers/HomeContainers/FeaturesSection/index.tsx
+++ src/containers/HomeContainers/FeaturesSection/index.tsx
@@ -1,81 +1,86 @@
-import { Grid, GridProps, Hidden } from '@mui/material'
-
-import Build from './assets/Build.svg'
-import Layout from './assets/Layout.svg'
-import Safety from './assets/Security.svg'
-import Unlock from './assets/Unlock.svg'
-import Vector from './assets/Vector.svg'
-import WeChat from './assets/Wechat.svg'
-
-import Carousel from 'components/Carousel'
+import { Grid, GridProps, Hidden } from '@material-ui/core'
 import PFeatureBlock from 'components/PFeatureBlock'
+import Safety from './assets/Security.svg'
+import Build from './assets/Build.svg'
+import Vector from './assets/Vector.svg'
+import Unlock from './assets/Unlock.svg'
+import Layout from './assets/Layout.svg'
+import WeChat from './assets/Wechat.svg'
+import Carousel from 'components/Carousel'
 
 const FeaturesSection = (props: GridProps) => {
   const blocks = [
     <PFeatureBlock
-      title="Secure System"
+      title="System Security"
       Icon={Safety}
-      buttonText="Learn about Parrot’s security"
+      buttonText="Our approach to system security"
       buttonLink="/docs"
       key="feature-1"
     >
-      Our tools are designed to be compatible with as many devices as possible via containerization
-      technologies like Docker or Podman.
+      The system is designed from the ground up to be secure. Native support for full disk encryption, blazing fast security updates
+      and a hardened debian core makes the system the perfect place to store sensitive data.
     </PFeatureBlock>,
     <PFeatureBlock
-      title="Portable"
+      title="Portability"
       Icon={Build}
-      buttonText="Learn about Parrot’s portability"
+      buttonText="Parrot runs anywhere"
       buttonLink="/docs"
       key="feature-2"
     >
-      Our tools are designed to be compatible with as many devices as possible via containerization
-      technologies like Docker or Podman.
+      Parrot is based on top of Debian, the most advanced and recognized universal operating system that can run anywhere.
+      From your laptop to your Phone, the Parrot core can be executed on a huge range of environments, including servers,
+      IoT boards, cloud containers (like docker and podman) and more.
     </PFeatureBlock>,
     <PFeatureBlock
       title="Performance"
       Icon={Vector}
-      buttonText="Learn about Parrot’s performance"
+      buttonText="The system is blazing fast"
       buttonLink="/docs"
       key="feature-3"
     >
-      Our tools are designed to be compatible with as many devices as possible via containerization
-      technologies like Docker or Podman.
+      We care about speed, and the system has been engineered to be as lightweight as possible.
+      You can run it on very old hardware, or perform very intensive tasks without performance drops.
+      Your applications, not the operating system, will be free to use all your hardware resources.
     </PFeatureBlock>,
     <PFeatureBlock
-      title="Privacy First"
+      title="Privacy"
       Icon={Unlock}
-      buttonText="Learn about Parrot’s security"
+      buttonText="We care about your privacy"
       buttonLink="/docs"
       key="feature-4"
     >
-      Our tools are designed to be compatible with as many devices as possible via containerization
-      technologies like Docker or Podman.
+      Don't leave your sensitive customer data floating around unprotected. The installer supports
+      full disk encryption, and the system lets you handle data in the most secure and reliable way.
+      Parrot OS is free from trackers and telemetry "features", and it provides many privacy tools
+      like AnonSurf, Tor Browser, a custom Firefox profile, easy cryptographic tools and much more.
     </PFeatureBlock>,
     <PFeatureBlock
-      title="Customizable"
+      title="Customizability"
       Icon={Layout}
-      buttonText="Learn about Parrot’s security"
+      buttonText="Customize the system"
       buttonLink="/docs"
       key="feature-5"
     >
-      Our tools are designed to be compatible with as many devices as possible via containerization
-      technologies like Docker or Podman.
+      Perfect default settings and great customizability are key to the success of the project.
+      You can either fall in love with the italian style of the Parrot system, or use it as a framework
+      to build your custom system tailored around your very own needs and style.
     </PFeatureBlock>,
     <PFeatureBlock
-      title="Free & Open Source"
+      title="Freedom"
       Icon={WeChat}
-      buttonText="Learn about Parrot’s security"
+      buttonText="Learn more about your rights"
       buttonLink="/docs"
       key="feature-6"
     >
-      Our tools are designed to be compatible with as many devices as possible via containerization
-      technologies like Docker or Podman.
+      The system is Free and Open Source, and all the code that powers the system is made available
+      either through our APT software repository or our GIT servers for you to read, customize and contribute to.
+      Free access to the code that runs on your devices is a fundamental freedom, and we do our best to
+      fight for your rights.g
     </PFeatureBlock>
   ]
   return (
     <Grid {...props} container item xs={12} md={9} spacing={4} justifyContent="center">
-      <Hidden mdDown>
+      <Hidden smDown>
         {blocks.map(el => (
           <Grid key={`grid-${el.key}`} item sm={6} lg={4}>
             {el}
