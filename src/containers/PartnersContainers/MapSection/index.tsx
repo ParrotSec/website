8b958d8378dde8b5b5c97f4a09ba34bfba027fc6
Vladyslav <vlad.dalechin@gmail.com>
2021-10-20 14:03:02 +0300

    fix leaflet and refactor paths

--- src/containers/PartnersContainers/MapSection/index.tsx
+++ src/containers/PartnersContainers/MapSection/index.tsx
@@ -2,33 +2,25 @@ import { makeStyles } from '@mui/styles'
 import L, { LatLngExpression } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-/*
-import marker2x from 'public/assets/leaflet/marker-icon-2x.png'
-import marker from 'public/assets/leaflet/marker-icon.png'
-import markerShadow from 'public/assets/leaflet/marker-shadow.png'
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: marker2x,
-  iconUrl: marker,
-  shadowUrl: markerShadow
-})
-*/
+import marker2x from 'assets/leaflet/marker-icon-2x.png'
+import marker from 'assets/leaflet/marker-icon.png'
+import markerShadow from 'assets/leaflet/marker-shadow.png'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 // eslint-disable-next-line no-underscore-dangle
 delete L.Icon.Default.prototype._getIconUrl
 
-L.Icon.Default.imagePath = 'public/assets/leaflet/'
-
-const icon = L.icon({
-  iconUrl: 'public/assets/leaflet/marker-icon.png',
-  iconRetinaUrl: 'public/assets/leaflet/marker-icon-2x.png',
-  shadowUrl: 'public/assets/leaflet/marker-shadow.png',
-  iconSize: [38, 95]
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: marker2x.src,
+  iconUrl: marker.src,
+  shadowUrl: markerShadow.src
 })
 
-L.Marker.prototype.options.icon = icon
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+// eslint-disable-next-line no-underscore-dangle
 
 const useStyles = makeStyles(theme => ({
   map: {
@@ -50,7 +42,7 @@ const MapSection = () => {
         attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={icon}>
+      <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
