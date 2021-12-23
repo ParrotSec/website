eb577d3ae070b051ce6681f493dbc08efe819929
danterolle <danterolle@catania.linux.it>
2021-12-23 18:28:48 +0100

    Update code with map, more split

---
+++ src/containers/ContributeContainers/GetInvolvedSection/contribute.ts
@@ -0,0 +1,35 @@
+import Github from 'containers/ContributeContainers/assets/github.svg'
+import Gitlab from 'containers/ContributeContainers/assets/gitlab.svg'
+import Milestones from 'containers/ContributeContainers/assets/milestone.svg'
+import Todo from 'containers/ContributeContainers/assets/todo.svg'
+
+export default [
+  {
+    title: 'Public Todo list',
+    description:
+      'Our tools are designed to be compatible with as many devices as possible via containerization technologies like Docker or Podman.',
+    url: 'https://nest.parrotsec.org/org/parrot/-/blob/master/README.md',
+    icon: Todo
+  },
+  {
+    title: 'Milestones',
+    description:
+      'Our tools are designed to be compatible with as many devices as possible via containerization technologies like Docker or Podman.',
+    url: 'https://nest.parrotsec.org/org/parrot/-/milestones',
+    icon: Milestones
+  },
+  {
+    title: 'NEST - GitLab instance',
+    description:
+      'Our tools are designed to be compatible with as many devices as possible via containerization technologies like Docker or Podman.',
+    url: 'https://nest.parrotsec.org',
+    icon: Gitlab
+  },
+  {
+    title: 'GitHub - backup mirror',
+    description:
+      'Our tools are designed to be compatible with as many devices as possible via containerization technologies like Docker or Podman.',
+    url: 'https://github.com/parrotsec',
+    icon: Github
+  }
+]
