abda9a3f04106e10116a9ceb24cb19cd38b8f951
danterolle <danterolle@catania.linux.it>
2022-01-12 13:31:09 +0100

    Changed head title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,9 +23,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <title>My App</title>
+        <title>Parrot Security</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
         <link rel="stylesheet" href="https://use.typekit.net/adk3ies.css" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <SwitchThemeProvider>
         <CssBaseline />
